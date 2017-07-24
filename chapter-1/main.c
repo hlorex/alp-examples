98bbff09412aee02c2c6ab069f4e6ddd76562fc4
Sergey Movchan <natan@list.ru>
2017-07-24 18:12:28 +0300

    First digit - 115

--- chapter-1/main.c
+++ chapter-1/main.c
@@ -1,8 +1,3 @@
-/***********************************************************************
-* Code listing from "Advanced Linux Programming," by CodeSourcery LLC  *
-* Copyright (C) 2001 by New Riders Publishing                          *
-* See COPYRIGHT for license information.                               *
-***********************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
--- test-01/main.c
+++ test-01/main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
-int d = 1;
 
 int main ()
 {
-    putc(d + '0', stdout);
-    putc('\n', stdout);
+    int a = 115, c, d, e, f, g;
 
-    return 0;
+    c = a / 100; // c = 1
+    d = a % 100; // d = 15
+    e = d / 10;  // e = 1
+    f = d % 10;  // f = 5
+
+    printf("Сотни:");
+    putc(0x30 + c, stdout);
+    putc('\n', stdout);
+    printf("Десятки:");
+    putc(0x30 + e, stdout);
+    putc('\n', stdout);
+    printf("Единицы:");
+    putc(0x30 + f, stdout);
+    putc('\n', stdout);
 }
