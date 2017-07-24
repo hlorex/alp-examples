956907843c86f30df7a8ab8b9f8c63eb832cb9eb
Sergey Movchan <natan@list.ru>
2017-07-24 18:58:40 +0300

    Input digit from console

--- test-01/main.c
+++ test-01/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
-int main ()
+int main (int argc, char *argv[])
 {
-    int a = 124124, c;
+    int a = 124124, c, i, x;
+
+    if (argc <= 1)
+        return 1;
+
+    char *str = argv[1];
+    int len = strlen(str);
+    for (a = 0, i = len - 1, x = 1; i > 0; x *= 10, i--)
+    {
+        a = a + str[i] * x;
+        putc(str[i], stdout);
+    }
+    putc('\n', stdout);
 
     for (c = 0; a > 0; a = a / 10)
     {
@@ -11,4 +24,6 @@ int main ()
 
     putc(0x30 + c, stdout);
     putc('\n', stdout);
+
+    return 0;
 }
