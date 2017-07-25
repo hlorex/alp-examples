f35833e6e0cf9eee07c5eaa60c1ae838962c0778
Sergey Movchan <natan@list.ru>
2017-07-25 21:22:23 +0300

    Fix input int from string

--- test-01/main.c
+++ test-01/main.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
+int str_to_int(const char *str)
+{
+    int a, i, x, len = strlen(str);
+    for (a = 0, i = len - 1, x = 1; i >= 0; x *= 10, i--)
+    {
+        a = a + (str[i] - 0x30) * x;
+    }
+    return a;
+}
+
 int main (int argc, char *argv[])
 {
-    int a = 124124, c, i, x;
+    int a, b, c, d, i, x, k, q;
 
     if (argc <= 1)
         return 1;
 
-    char *str = argv[1];
-    int len = strlen(str);
-    for (a = 0, i = len - 1, x = 1; i > 0; x *= 10, i--)
-    {
-        a = a + str[i] * x;
-        putc(str[i], stdout);
-    }
-    putc('\n', stdout);
+    a = str_to_int(argv[1]);
+    q = a;
 
-    for (c = 0; a > 0; a = a / 10)
+    for (k = 1, c = 0; a > 0; a = a / 10)
     {
+        k *= 10;
         c++;
     }
+    k = k / 10;
+    printf("%d, %d, %d\n", c, k, q);
+    for (d = 0; c >= 0; k/=10)
+    {
+        d = q / k;
+        q = q % 10;
+        c--;
+        putc(0x30 + d, stdout);
+        putc('\n', stdout);
+    }
 
-    putc(0x30 + c, stdout);
     putc('\n', stdout);
 
     return 0;
